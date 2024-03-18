c13ecee4397c4c9c5cb7003432d6c9e82d7b3532
jess.wang <jess.wang@ace.io>
2024-03-18 11:38:56 +0800

    Fix

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use num_complex::Complex;
 // 引入 web.rs 模块
 mod web;
 mod option;
-mod escapeTime;
+mod escape_time;
 
 fn main() {
     println!("Jess Demo!");
     //web::run();
-    println!("{:?}", escapeTime::escape_time(Complex::new(3.00, 0.00), 5).unwrap());
+    println!("{:?}", escape_time::escape_time(Complex::new(3.00, 2.00), 5).unwrap());
 }
